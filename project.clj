2552f43bbe37e73b42df6d1d4f4cddedbdf360a1
Kevin Albrecht <onlyafly@gmail.com>
2013-04-12 19:42:02 +0200

    Updated project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject waltz "0.1.0-alpha2"
+(defproject onlyafly/waltz "0.1.0-alpha2"
   :description "A ClojureScript library to keep your state in step"
   :url "http://github.com/onlyafly/waltz"
   :license {:name "Eclipse Public License"
